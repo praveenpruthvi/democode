acfef25c5a6e54a37b601c36489bb1596a871040
Praveen <praveen.hb@impelsys.com>
2019-11-18 07:07:49 +0000

    Commit 20191118 -3

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -24,7 +24,7 @@ class Welcome extends CI_Controller {
 	public function userLogin() {
 		$this->load->view('welcome_message');
 	}
-	public function userLogin191118_2() {
+	public function userLogin191118_3() {
 		$this->load->view('welcome_message');
 	}
 }
